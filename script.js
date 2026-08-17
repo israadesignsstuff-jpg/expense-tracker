80fa1470e14a517cb9d628386a5db25d6cf4e598
Israa Mohamed Ali <israadesignsstuff@gmail.com>
2026-08-17 19:57:19 +0300

    Added some DOM elements

---
+++ script.js
@@ -0,0 +1,9 @@
+const balanceEl = document.getElementById("balance");
+const incomeAmountEl = document.getElementById("income-amount");
+const expenseAmountEl = document.getElementById("expense-amount");
+const transactionListEl = document.getElementById("transaction-list");
+const transactionFormEl = document.getElementById("transaction-form");
+const descriptionEl = document.getElementById("description");
+const amountEl = document.getElementById("amount");
+
+let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
